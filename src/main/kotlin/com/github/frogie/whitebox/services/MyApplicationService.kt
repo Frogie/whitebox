53fff0f8e6044bb3a27e8593fe5522fc1c696a23
GitHub Action <action@github.com>
2020-12-03 07:36:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/frogie/whitebox/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/frogie/whitebox/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.frogie.whitebox.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.frogie.whitebox.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/frogie/whitebox/services/MyProjectService.kt
+++ src/main/kotlin/com/github/frogie/whitebox/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.frogie.whitebox.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.frogie.whitebox.MyBundle
 
 class MyProjectService(project: Project) {
 
